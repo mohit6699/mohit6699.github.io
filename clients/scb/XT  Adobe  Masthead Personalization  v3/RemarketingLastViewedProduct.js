d3bfaf2a35088cbdf2c4529e23251f467f29e260
mohit26394 <mohittiwari26394@gmail.com>
2022-09-02 00:48:57 +0530

    Added profile script

--- clients/scb/XT  Adobe  Masthead Personalization  v3/RemarketingLastViewedProduct.js
+++ clients/scb/XT  Adobe  Masthead Personalization  v3/RemarketingLastViewedProduct.js
@@ -1,5 +1,3 @@
-
-<script>
 if ((page.url.indexOf("https://www.sc.com/sg/save/savings-accounts/esaver/") >= 0) ||(page.url.indexOf("https://www.sc.com/sg/save/current-accounts/wealth-saver/") >= 0) || (page.url.indexOf("https://www.sc.com/sg/save/current-accounts/bonussaver/") >= 0) || (page.url.indexOf("https://www.sc.com/sg/credit-cards/smart-credit-card/") >= 0) || (page.url.indexOf("https://www.sc.com/sg/wealth/") >= 0) || (page.url.indexOf("https://www.sc.com/sg/borrow/loans/balance-transfer/") >= 0) || (page.url.indexOf("https://www.sc.com/sg/borrow/loans/cashone/") >= 0)) {
     if ((page.url.indexOf("https://www.sc.com/sg/save/savings-accounts/esaver") >= 0))
         return "ESAVER";
@@ -25,7 +23,3 @@ else {
         }
     }
 }
-</script>
-
-https://www.sc.com/sg/save/savings-accounts/esaver/?subChanCode=IBSC&referID=IBAT&instance=IBATBS
-https://www.sc.com/sg/save/current-accounts/wealth-saver/?subChanCode=IBSC&referID=IBAT&instance=IBATBS
